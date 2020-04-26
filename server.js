c091d8ed14c68b9ee0cbdb1d8a4b8e417e61a4db
Cameron Wise <wisecameron1@gmail.com>
2020-04-25 22:09:20 -0400

    new folder structure and links to stylesheet and js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/ws.html');
-})
+app.use('/', express.static(__dirname + '/client'));
+
 
 app.listen(3000, function() {
     console.log('App listening on port 3000!')
