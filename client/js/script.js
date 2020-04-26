58f037fe722835bb5e352cc2fad535180d82bd79
Cameron Wise <wisecameron1@gmail.com>
2020-04-26 19:51:19 -0400

    new StrokesObject structure to send different colors

--- client/js/script.js
+++ client/js/script.js
@@ -8,12 +8,13 @@ const blueBrush = document.getElementById("blue");
 const blackBrush = document.getElementById("black");
 const context = canvas.getContext('2d');
 
-let strokesArray = [];
+let strokesArray = {};
+
 
 var ws = new WebSocket('ws://localhost:40510')
 
 $('#blue').on('click', function(event){
-    color = "#FF0000"
+    color = '#3636e7'
 });
 
 
@@ -22,17 +23,19 @@ const rect = canvas.getBoundingClientRect();
 ws.onmessage = function (msg){
     let message = JSON.parse(msg.data)
     console.log(message);
-    if(message[0].x1 != null){
+    if(message.events[0].x1 != null){
 
-        console.log(message[0].x1);
-        for(i = 0; i < message.length; i++){
-            console.log(message[i].x1+ "hello")
-            drawLine(context, message[i].x1, message[i].y1, message[i].x2, message[i].y2);
+        for(i = 0; i < message.events.length; i++){
+            console.log(message.events[i].x1+ "hello")
+            drawLine(context, message.events[i].x1, message.events[i].y1, message.events[i].x2, message.events[i].y2, message.color);
         }
     }
 }
 
 canvas.addEventListener('mousedown', e => {
+    strokesArray.events = [];
+
+    strokesArray.color = color;
     x = e.clientX - rect.left;
     y = e.clientY - rect.top;
     Drawing = true;
@@ -40,7 +43,7 @@ canvas.addEventListener('mousedown', e => {
 
 canvas.addEventListener('mousemove', e => {
     if(Drawing === true) {
-        drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top);
+        drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top, color);
         x = e.clientX - rect.left;
         y = e.clientY - rect.top;
     }
@@ -48,12 +51,13 @@ canvas.addEventListener('mousemove', e => {
 
 canvas.addEventListener('mouseup', e => {
     if(Drawing === true){
-        drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top);
-        for(item of strokesArray){
+        drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top, color);
+        for(item of strokesArray.events){
             console.log(item.x1);
         }
         ws.send(JSON.stringify(strokesArray));
-        strokesArray = [];
+        strokesArray.events = [];
+        strokesArray.color = ""
         x = 0;
         y = 0; 
         Drawing = false;
@@ -61,18 +65,21 @@ canvas.addEventListener('mouseup', e => {
     }
 });
 
-function drawLine(context, x1, y1, x2, y2){
+function drawLine(context, x1, y1, x2, y2, drawcolor){
     context.beginPath();
-    context.strokeStyle = color;
+    context.strokeStyle = drawcolor;
     context.lineWidth = 2;
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
-    strokesArray.push({
-        x1:x1,
-        y1:y1,
-        x2:x2,
-        y2:y2,
-    })
+    if (Drawing === true){
+        strokesArray.events.push({
+            x1:x1,
+            y1:y1,
+            x2:x2,
+            y2:y2,
+        })
+    }
+
     context.stroke();
     context.closePath();
 }
