ef56ef3d0faf618f6504970f6ad2400f426ab6b1
Cameron Wise <wisecameron1@gmail.com>
2020-05-06 23:59:09 -0400

    server.js rewrite for custom canvases based on IDs

--- client/js/script.js
+++ client/js/script.js
@@ -6,13 +6,14 @@ let color = 'black'
 const canvas = document.getElementById("draw-canvas");
 const context = canvas.getContext('2d');
 const chatBox = document.getElementById("chat-list");
+let username = ($('#username-input').val());
 let strokesArray = {};
 strokesArray.type = "drawing";
-let username = ""
+session_id = '';
 
 var ws = new WebSocket('ws://localhost:40510')
 
-$('#blue').on('click', function(event){
+$('#blue').on('click', function(event){//button functions for different colors
     color = '#3636e7'
 });
 $('#black').on('click', function(event){
@@ -25,17 +26,17 @@ $('#green').on('click', function(event){
     color = 'green'
 });
 
-$(document).click(function(e) {
+$(document).click(function(e) {// will not continue drawing if off the canvas
     var target = $(e.target);
 
     if( !target.is("#draw-canvas") && target.closest("#draw-canvas").length === 0 ) {
         Drawing = false;
     }
 });
-$('#chat-form').submit(function() { 
+
+$('#chat-form').submit(function() { //sends chat to server
     var chatValue = ($('#chat-input').val());
-    console.log(chatValue);
-    if(chatValue != ""){
+    if(chatValue != ""){//making sure empty chats are not sent
         ws.send(JSON.stringify({
             user: username,
             type: "chat",
@@ -45,38 +46,47 @@ $('#chat-form').submit(function() {
     }
     return false;    
 });
-$('#username-form').submit(function(){
+
+$('#username-form').submit(function(){//sets username
     username = ($('#username-input').val());
-    console.log(username);
-    return false;
+    return false;//return false or else it attempts to reload the client
 })
 
 
 
 
 const rect = canvas.getBoundingClientRect();
-
+ws.onopen = function(){
+    url = window.location.href
+    id = url.split('/')[4];
+    console.log(id);
+    session_id = id;
+    ws.send(JSON.stringify({
+        type: 'initialize_client',
+        session_id: id
+    }));
+}
 ws.onmessage = function (msg){
     let messages = JSON.parse(msg.data)
     console.log(messages);
     
     
-    if(messages.type === "catchup"){
+    if(messages.type === "catchup"){//getting previous drawing events
+        console.log(msg)
         for(message of messages.all_draw_events){
-            console.log(message)
             for(i = 0; i < message.events.length; i++){
                 console.log(message.events[i].x1+ "hello")
                 drawLine(context, message.events[i].x1, message.events[i].y1, message.events[i].x2, message.events[i].y2, message.color);
                 }
             }
-    } else if(messages.type === "drawing"){
+    } else if(messages.type === "drawing"){//getting new drawings
         for(message of messages.draw_event.events){
             console.log(message+ "hello")
             drawLine(context, message.x1, message.y1, message.x2, message.y2, messages.draw_event.color);
         }
-    } else if (messages.type === "chat"){
+    } else if (messages.type === "chat"){//new chats
         chatBox.insertAdjacentHTML("beforeend", "<div class = 'message'> <div class = 'user'>"+ messages.username+":</div> <div class = 'message'><p>" +messages.chat_message+ "</p></div></div>");
-    } else if (messages.type = "chat_logs"){
+    } else if (messages.type = "chat_logs"){//getting chat logs
         for(chat of messages.chat_logs){
             chatBox.insertAdjacentHTML("beforeend", "<div class = 'message'> <div class = 'user'>"+ chat.user+":</div> <div class = 'message'><p>" +chat.chat_message+ "</p></div></div>");
         }
@@ -85,16 +95,16 @@ ws.onmessage = function (msg){
 
 }
 
-canvas.addEventListener('mousedown', e => {
+canvas.addEventListener('mousedown', e => {//on mouse down start a new events array and begin drawing
     strokesArray.events = [];
-
+    strokesArray.session_id = session_id;
     strokesArray.color = color;
     x = e.clientX - rect.left;
     y = e.clientY - rect.top;
     Drawing = true;
 });
 
-canvas.addEventListener('mousemove', e => {
+canvas.addEventListener('mousemove', e => {//get x and y values everytime the client moves
     if(Drawing === true) {
         drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top, color);
         x = e.clientX - rect.left;
@@ -102,7 +112,7 @@ canvas.addEventListener('mousemove', e => {
     }
 });
 
-canvas.addEventListener('mouseup', e => {
+canvas.addEventListener('mouseup', e => {//stop drawing and clear the events and color
     if(Drawing === true){
         drawLine(context, x, y, e.clientX - rect.left, e.clientY - rect.top, color);
         ws.send(JSON.stringify(strokesArray));
@@ -115,7 +125,7 @@ canvas.addEventListener('mouseup', e => {
     }
 });
 
-function drawLine(context, x1, y1, x2, y2, drawcolor){
+function drawLine(context, x1, y1, x2, y2, drawcolor){//strokes and sending them to the events 
     context.beginPath();
     context.strokeStyle = drawcolor;
     context.lineWidth = 2;
--- server.js
+++ server.js
@@ -1,70 +1,66 @@
-var express = require('express')
+const express = require('express')
+const path = require('path');
+const WebSocetServer = require('ws').Server
+const {v1 : uuidv1} = require('uuid');
+
 var app = express()
 
-let canvasStrokes = [];
-let chatLogs = [];
 app.use('/', express.static(__dirname + '/client'));
+app.get('/', function(req, res) {
+    // Create a unique id
+    // Redirect them to /draw/id
+    var id = uuidv1();
+    res
+        .set('Cache-Control', 'public, max-age=0')
+        .redirect(302, `/draw/${id}`);
+    
+});
 
+app.get('/draw/:id', function(req, res) {
+    res.sendFile(__dirname + '/client/app.html');
+}); // Server for ids
 
-app.listen(3000, function() {
-    console.log('App listening on port 3000!')
+app.listen(3000, function(){
+    console.log('app listening at port 3000');
 })
 
-const WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({port: 40510})
-    CLIENTS = [];
+wss = new WebSocetServer({port:40510})
 
-wss.on('connection', function(ws) {
-    
-    CLIENTS.push(ws);
-    
-    ws.send(JSON.stringify({
-        type: "catchup",
-        all_draw_events: canvasStrokes
-    }));
-    ws.send(JSON.stringify({
-        type: "chat_logs",
-        chat_logs: chatLogs
+// Basic structure
+var SESSIONS = {
+    "sessionID": {
+        clients: [],
+        strokes: [],
+        chat_messages: []
+    }
+}
 
-    }))
-    console.log(canvasStrokes);
-    ws.on('message', function (message, sender) {
+wss.on('connection', function(ws){
+    ws.on('message', function(message, sender){
+        let msg = JSON.parse(message);
         
-        let msg = JSON.parse(message);//this is correct do not touch
+        if (msg.type == "initialize_client"){
+            if (!SESSIONS[msg.session_id]) {
+                SESSIONS[msg.session_id] = {
+                    clients:[],
+                    strokes:[],
+                    chat_messages:[]
+                }
+            }
+            ws.send(JSON.stringify({
+                type: "catchup",
+                all_draw_events: SESSIONS[msg.session_id].strokes
+            }));
+        }
+
+        else if (msg.type == "drawing"){
+            SESSIONS[msg.session_id].strokes.push({
+                events: msg.events,
+                color: msg.color,
+
+            });
+        }
 
         console.log(msg);
-        
-
-        if (msg.type === "drawing"){
-            canvasStrokes.push(msg);
-            sendAllButSender(JSON.stringify({
-                type: "drawing",
-                draw_event: msg
-            }), ws);//sends to all excluding sender 
-        } 
-        else if (msg.type === "chat"){
-            chatLogs.push(msg);
-            console.log("chat recieved");
-            sendAll(JSON.stringify({
-                username: msg.user,
-                type: "chat",
-                chat_message: msg.chat_message
-            }))
-        } 
-  
     })
-
-
 })
-function sendAllButSender (message, sender) {
-    for (var i=0; i<CLIENTS.length; i++) {
-        if(CLIENTS[i] !== sender){
-            CLIENTS[i].send(message);
-        }
-    }
-}
-function sendAll (message) {
-    for (var i=0; i<CLIENTS.length; i++) {
-        CLIENTS[i].send(message);
-    }
-}
